d77c90ac8571c1dcf549f820b60c0a380fcf88e4
BManoj04 <bmanoj2364@gmail.com>
2023-09-28 16:27:27 +0530

    Front-end

---
+++ favorits.js
@@ -0,0 +1,3 @@
+favList = ["65129b61d6d231bd0f5ea1ba","65129c4a5d5c40ef2c4efef1"]
+
+module.exports = favList;
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express =  require ("express")
 const ejsmate = require("ejs-mate")
 const path = require("path")
-const mongoose = require("mongoose")
-const jsonUtils = require("./json_utils");  
+const mongoose = require("mongoose")  
 const detail = require("./models/info")
+const favi = require("./favorits")
 const app = express()
 
 mongoose.connect('mongodb://127.0.0.1:27017/Tourist')
@@ -23,7 +23,8 @@ app.set("view engine", "ejs")
 
 app.get("/", async (req,res)=>{
     const details = await detail.find({}) 
-    res.render("home.ejs", { details : JSON.stringify(details) })
+    const dbDatas = await detail.find({})
+    res.render("home.ejs", { details : JSON.stringify(details) , favi , dbDatas })
 })
 
 app.get("/:id",(req,res)=>{
