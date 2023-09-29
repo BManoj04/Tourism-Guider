2f9d15f76263071e4ac159e2ed2a7af46d85536e
BManoj04 <bmanoj2364@gmail.com>
2023-09-29 19:49:29 +0530

    front and back end

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose")
 const detail = require("./models/info")
 const favi = require("./favorits")
 const app = express()
-
+https://dribbble.com/
 mongoose.connect('mongodb://127.0.0.1:27017/Tourist')
 .then(() =>{
     console.log("Mongo connected")
@@ -27,11 +27,15 @@ app.get("/", async (req,res)=>{
     res.render("home.ejs", { details : JSON.stringify(details) , favi , dbDatas })
 })
 
-app.get("/:id",(req,res)=>{
-    let id = req.params["id"]
-    res.render("place.ejs",{ id })
+app.get("/:id",async(req,res)=>{
+    let id = req.params["id"].toString()
+    const details = await detail.find({})
+    const dbData = await detail.findOne({_id:id})
+    res.render("place.ejs",{ details : JSON.stringify(details), id ,dbData})
 })
-
+app.use(() =>{
+    console.log("manoj")
+})
 
 app.listen(3000,()=>{
 })
